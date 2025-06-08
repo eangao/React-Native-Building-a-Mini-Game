e02323cf835ac1aac9927c76b445131601797e95
eangao <elmarcera@gmail.com>
2025-06-08 16:49:08 +0800

    Adding & Using Custom Fonts with React Native Apps

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { ImageBackground, StyleSheet, SafeAreaView } from "react-native";
-
 import { LinearGradient } from "expo-linear-gradient"; // Need to intsll -> expo install expo-linear-gradient
+import { useState } from "react";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 import StartGameScreen from "./screens/StartGameScreen";
-import { useState } from "react";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
 import GameOverScreen from "./screens/GameOverScreen";
@@ -11,6 +12,15 @@ import GameOverScreen from "./screens/GameOverScreen";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+
+  const [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
